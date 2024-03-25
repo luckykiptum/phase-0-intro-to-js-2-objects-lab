15df927d4de3e07819fe92d995e1301cf8865867
lucky <luckykiptum@gmail.com>
2024-03-25 09:49:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+
+  
+
+const employee = {
+    name: "Lucky",
+
+    streetAddress: "Mai Mahiu Rd"
+};
+
+function updateEmployeeWithKeyAndValue(employee,name,Lucky){
+
+return{
+      ... employee,[name] : Lucky
+}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee){
+
+    const { name, ...newemployee} = employee;
+
+    return newemployee;
+}
+function  destructivelyDeleteFromEmployeeByKey(employee){
+
+    employee.name = undefined;
+    return employee;
+
+}
+
